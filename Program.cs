5f7c3034192b28409b2b2005a8649bcf8abb5ddb
Ilnur Garrapov <garrapovilnur@gmail.com>
2023-02-13 13:09:12 +0300

    fix_diagramm

--- Program.cs
+++ Program.cs
@@ -2,12 +2,6 @@
 "-2", "computer science", "Russia", "Denmark", "Kazan", "GB", "GBU" };
 int count = ArrayCheck(array);
 string[] resultArray = FillResultArray(array, count);
-void PrintArray(string[] array)
-{
-    Console.Write("[");
-    for (int i = 0; i < array.Length - 1; i++) Console.Write($"{array[i]}, ");
-    Console.Write($"{array[array.Length - 1]}]");
-}
 int ArrayCheck(string[] array)
 {
     int count = 0;
