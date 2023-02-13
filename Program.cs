aa8929e6fdbcfe6297536bdb729808bae3c9c3ec
Ilnur Garrapov <garrapovilnur@gmail.com>
2023-02-13 11:13:46 +0300

    print_array_add

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿string[] array = { "hello", "2", "world", ":-)", "1234", "1567", "Rus",
+"-2", "computer science", "Russia", "Denmark", "Kazan", "GB", "GBU" };
+int count = ArrayCheck(array);
+string[] resultArray = FillResultArray(array, count);
+void PrintArray(string[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length - 1; i++) Console.Write($"{array[i]}, ");
+    Console.Write($"{array[array.Length - 1]}]");
+}
